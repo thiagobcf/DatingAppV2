b700ea2171f96966a545d916f2b3dc32e76c1ab2
thiagobcf <thiagobcfonseca@gmail.com>
2024-06-28 12:26:10 -0300

    add: Creating the Angular application

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c7691252c810f5fa949cb33150907da91a496f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d70a1bef45fe76a0f45e2a97d181535e0c3fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
