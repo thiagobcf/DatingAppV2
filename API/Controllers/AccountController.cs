7f01d7ad1c6f40eb461b52bc12a8ee9fba992364
thiagobcf <thiagobcfonseca@gmail.com>
2024-07-02 10:50:43 -0300

    add: login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +26,26 @@ public class AccountController(DataContext context) : BaseApiController
 
     context.Users.Add(user);
     await context.SaveChangesAsync();
+
+    return user;
+  }
+
+  [HttpPost("login")]
+  public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+  {
+    var user = await context.Users.FirstOrDefaultAsync(x => 
+      x.UserName == loginDto.Username.ToLower());
+
+    if (user == null) return BadRequest("Invalid username");
+
+    using var hmac = new HMACSHA512(user.PasswordSalt);
+
+    var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+    for (int i = 0; i < ComputeHash.Length; i++)
+    {
+      if (ComputeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+    }
 
     return user;
   }
