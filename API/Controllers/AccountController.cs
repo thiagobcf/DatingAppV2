b511bea43a0c672178e6ffa5aad9cf2b5bb34a69
thiagobcf <thiagobcfonseca@gmail.com>
2024-07-02 12:48:28 -0300

    add: creating User DTO and return the token.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,10 +8,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
   [HttpPost("register")]         // account/register
-  public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+  public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
   {
     if (await UserExists(registerDto.Username)) return BadRequest("username is taken");
     
@@ -27,11 +27,15 @@ public class AccountController(DataContext context) : BaseApiController
     context.Users.Add(user);
     await context.SaveChangesAsync();
 
-    return user;
+    return new UserDto
+    {
+      Username = user.UserName,
+      Token = tokenService.CreateToken(user)
+    };
   }
 
   [HttpPost("login")]
-  public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+  public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
   {
     var user = await context.Users.FirstOrDefaultAsync(x => 
       x.UserName == loginDto.Username.ToLower());
@@ -47,7 +51,11 @@ public class AccountController(DataContext context) : BaseApiController
       if (ComputeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
     }
 
-    return user;
+    return new UserDto
+    {
+      Username = user.UserName,
+      Token = tokenService.CreateToken(user)
+    };
   }
 
   private async Task<bool> UserExists(string username)
