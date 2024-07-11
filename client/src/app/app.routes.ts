63fd5d37c52960d16569fe9b64597c1aa88b9b0c
thiagobcf <thiagobcfonseca@gmail.com>
2024-07-11 11:43:26 -0300

    add: Routing: creating components: member-list; member-detail; list; messages.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
-export const routes: Routes = [];
+
+// definição de rotas
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'members', component: MemberListComponent},
+  {path: 'members/:id', component: MemberDetailComponent},                // rota dinâmica
+  {path: 'lists', component: ListsComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: '**', component: HomeComponent, pathMatch: 'full'}              // caso nenhuma rota coincidir.
+];
