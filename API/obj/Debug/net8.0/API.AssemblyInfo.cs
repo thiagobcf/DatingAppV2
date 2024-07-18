23a1779387ac170b3d6f60c38118377830b09b7c
thiagobcf <thiagobcfonseca@gmail.com>
2024-07-18 18:03:52 -0300

    add: Seeding data.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e761328d9aa97cabb60fb2d0d714a93905be64f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629dc63cb1f5dbbdf4e6f455c1828d4e99867e80")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
