e9e7dbcff9818d361d53d7ca0c9b31e943e2c8a9
thiagobcf <thiagobcfonseca@gmail.com>
2024-07-19 13:09:04 -0300

    add: Updating the user controller and add a Dto for Members.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,23 +5,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
+[Authorize]
 
-public class UsersController(DataContext context) : BaseApiController
-{
-  [AllowAnonymous]
+public class UsersController(IUserRepository userRepository) : BaseApiController
+{  
   [HttpGet]
   public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
   {
-    var users = await context.Users.ToListAsync();
+    var users = await userRepository.GetUsersAsync();
 
-    return users;
+    return Ok(users);
   }
-
-  [Authorize]
-  [HttpGet("{id:int}")]  // api/users/id
-  public async Task<ActionResult<AppUser>> GetUser(int id)
+  
+  [HttpGet("{username}")]  // api/users/id
+  public async Task<ActionResult<AppUser>> GetUser(string username)
   {
-    var user = await context.Users.FindAsync(id);
+    var user = await userRepository.GetUserByUsernameAsync(username);
 
     if (user == null) return NotFound();
 
