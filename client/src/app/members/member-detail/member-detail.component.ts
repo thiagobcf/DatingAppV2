a809bb870d53f3506a47e2b5fe6e0f79d058c2a3
thiagobcf <thiagobcfonseca@gmail.com>
2024-07-26 12:24:43 -0300

    add: photo gallery.

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,13 +1,14 @@
-import { Component, inject, type OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryModule, ImageItem, type GalleryItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -15,6 +16,7 @@ export class MemberDetailComponent implements OnInit {
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
   member?: Member;      // verificar depois a questao de ser opcional.
+  images: GalleryItem[] = [];
 
   ngOnInit(): void {
       this.loadMember()
@@ -24,7 +26,12 @@ export class MemberDetailComponent implements OnInit {
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member
+        member.photos.map(p => {
+          this.images.push(new ImageItem({src: p.url, thumb: p.url}))
+        })
+      }
     })
   }
 }
