0d24766f1b1be96c940fd2a561afa906b774e47a
thiagobcf <thiagobcfonseca@gmail.com>
2024-08-16 13:18:07 -0300

    add: Creating a reusable text imput.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3186227e18231db42adb1d159e7573d197c14a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc75b43382f15ffd0ef89431fbce7988db42dfec")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
