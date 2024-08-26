fb61746999864e5ad4de6714fff0088013a85b0b
thiagobcf <thiagobcfonseca@gmail.com>
2024-08-26 12:32:25 -0300

    add: helper classes for pagination.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd328b049866451f51f82da72e22514b7809ac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+885715abb13c023e6986d61dfd801106bd8cd417")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
