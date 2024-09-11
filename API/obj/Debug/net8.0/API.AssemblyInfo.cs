4426be36dc6619c9ce04053e2a342cf649103d03
thiagobcf <thiagobcfonseca@gmail.com>
2024-09-11 12:48:24 -0300

    add: Controller methods

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb453a393ddc6cdb3ebaaf8a909b5239dca3a81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e06e68d1318287c7aa4cc359297f9f949738d43")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
