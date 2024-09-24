81a14f713fbd7f2b41cf21d1075850d68e5a2f5c
thiagobcf <thiagobcfonseca@gmail.com>
2024-09-24 12:50:03 -0300

    add: Fixing the photo weirdness, and Deleting messages on the API.

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -34,9 +34,12 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
 
         query = messageParams.Container switch 
         {
-            "Inbox" => query.Where(x => x.Recipient.UserName == messageParams.Username),
-            "Outbox" => query.Where(x => x.Sender.UserName == messageParams.Username),
-            _ => query.Where(x => x.Recipient.UserName == messageParams.Username && x.DateRead == null)
+            "Inbox" => query.Where(x => x.Recipient.UserName == messageParams.Username 
+                && x.RecipientDeleted == false),
+            "Outbox" => query.Where(x => x.Sender.UserName == messageParams.Username 
+                && x.SenderDeleted == false),
+            _ => query.Where(x => x.Recipient.UserName == messageParams.Username 
+                && x.DateRead == null && x.RecipientDeleted == false)
         }; 
 
         var message = query.ProjectTo<MessageDto>(mapper.ConfigurationProvider); 
@@ -50,7 +53,9 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
             .Include(x => x.Sender).ThenInclude(x => x.Photos)
             .Include(x => x.Recipient).ThenInclude(x => x.Photos)
             .Where(x => x.RecipientUsername == currentUsername 
+                && x.RecipientDeleted == false
                 && x.Sender.UserName == recipientUsername || x.SenderUsername == currentUsername
+                && x.SenderDeleted == false
                 && x.RecipientUsername == recipientUsername)
             .OrderBy(x => x.MessageSent)
             .ToListAsync();
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b57b58c84d732dd71b338cec648bad4dc33ae50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ad87593bb5078afce4910b4c47ba3d558dd707c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
