b1df8bc780c46df417b9c6e4fe17ecc2962b25bd
thiagobcf <thiagobcfonseca@gmail.com>
2024-09-26 12:42:21 -0300

    add: Configuring the DbContext and startup class.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+467042ce52a2bb5def1be44a18c6efe03bb0ebd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66a2127880bcd283dc670e006e51da9d8749b3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
