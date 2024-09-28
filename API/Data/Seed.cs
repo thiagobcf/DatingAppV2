4cf2fff7134397e1890ea0bf294cf06f56ffe18f
thiagobcf <thiagobcfonseca@gmail.com>
2024-09-28 13:06:43 -0300

    add: Updating the seed method.

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -2,15 +2,16 @@
 using System.Text;
 using System.Text.Json;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class Seed
 {
-  public static async Task SeedUsers(DataContext context)
+  public static async Task SeedUsers(UserManager<AppUser> userManager)
   {
-    if (await context.Users.AnyAsync()) return;
+    if (await userManager.Users.AnyAsync()) return;
 
     var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
 
@@ -21,12 +22,8 @@ public class Seed
     if (users == null) return;
 
     foreach (var user in users)
-    {
-      using var hmac = new HMACSHA512();            
-
-      context.Users.Add(user);
-    }
-
-    await context.SaveChangesAsync();
+    {            
+      await userManager.CreateAsync(user, "Pa$$w0rd");   
+    }    
   }
 }
