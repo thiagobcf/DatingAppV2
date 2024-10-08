423d373aaf3586b05d0a43876bc25fdf0d347b99
thiagobcf <thiagobcfonseca@gmail.com>
2024-10-08 17:38:10 -0300

    add: Editing user roles, admin component.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72849d174ece3c296a2fc12ad4b8aae2c398b97c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ce8d3d676ac312dfbd3cd90512348fc844473e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
