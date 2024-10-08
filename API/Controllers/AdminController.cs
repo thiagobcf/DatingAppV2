67ce8d3d676ac312dfbd3cd90512348fc844473e
thiagobcf <thiagobcfonseca@gmail.com>
2024-10-08 13:01:24 -0300

    add: policy based authorisation, Getting the users with roles.

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+using System;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AdminController(UserManager<AppUser> userManager) : BaseApiController
+{
+  [Authorize(Policy = "RequireAdminRole")]
+  [HttpGet("users-with-roles")]
+  public async Task<ActionResult> GetUsersWithRoles()
+  {
+    var users = await userManager.Users
+      .OrderBy(x => x.UserName)
+      .Select(x => new
+      {
+        x.Id,
+        Username = x.UserName,
+        Roles = x.UserRoles.Select(r => r.Role.Name).ToList()
+      }).ToListAsync();
+    return Ok(users);
+  }
+
+  [Authorize(Policy = "ModeratePhotoRole")]
+  [HttpGet("photos-to-moderate")]
+  public ActionResult GetPhotosForModeration()
+  {
+    return Ok("adins and moderators can see this");
+  }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6173c48da1ab7bcf37337a182e9aedbd68eb5c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72849d174ece3c296a2fc12ad4b8aae2c398b97c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
